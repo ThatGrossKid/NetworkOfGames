1ece97ebae801881b919eb779abca3ad7b3962c4
Jonathan Gross <60676525+ThatGrossKid@users.noreply.github.com>
2021-10-24 14:28:14 -0400

    Added the main website
    Added all the main HTMLs and scripts written up to this point to have a working website.

---
+++ account-page.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#unBtn").click(function(){
+        $("#change").show();
+    });
+    $("#eBtn").click(function(){
+        $("#change").hide();
+    });
+    $("#pBtn").click(function(){
+        $("#change").show();
+    });
+    $("#changeBTN").click(function(){
+        
+        $("#change").hide();
+    });
+})
